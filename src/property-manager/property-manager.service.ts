b88153bd41d1607f187bcd250b7da53f7635442c
kastroud <johnsonolaolu@gmail.com>
2026-01-13 06:34:52 +0100

    chore: update eslint to version 9.39.2 and add eslint-disable directives in service files

--- src/property-manager/property-manager.service.ts
+++ src/property-manager/property-manager.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreatePropertyManagerDto } from './dto/create-property-manager.dto';
 import { UpdatePropertyManagerDto } from './dto/update-property-manager.dto';
--- src/tenant/tenant.service.ts
+++ src/tenant/tenant.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { CreateTenantDto } from './dto/create-tenant.dto';
 import { UpdateTenantDto } from './dto/update-tenant.dto';
