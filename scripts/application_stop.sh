832281e4c716dd97df82e16ddf4812bf2d6ee5ed
kastroud <johnsonolaolu@gmail.com>
2026-01-13 16:28:51 +0100

    chore: remove obsolete deployment scripts and configuration files

--- scripts/application_stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Stopping existing node servers
-echo "Stopping any existing node servers"
-sudo pm2 stop all || true
-# pkill node
--- scripts/validate_service.sh
+++
@@ -1,4 +0,0 @@
-# # Replace with a command to check your application's health, e.g., curl request to your app's health endpoint
-# curl -f http://localhost:3000/ || exit 1
-
-echo "Application is running."
