83ecc9c54d24ae6e94150956add6c6487a5c0b28
kastroud <johnsonolaolu@gmail.com>
2026-02-20 18:55:28 +0100

    feat: implement email notification queue and worker for asynchronous email processing

--- src/notification/email/email.module.ts
+++ src/notification/email/email.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { EmailWorker } from './email.worker';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from 'src/config/env.config';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { join } from 'path';
+import { BullModule } from '@nestjs/bullmq';
+import { JOB_NAMES } from 'src/utils/constants';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
 
 @Module({
   imports: [
@@ -41,8 +46,18 @@ import { join } from 'path';
         },
       }),
     }),
+    BullModule.registerQueue({
+      name: JOB_NAMES.EMAIL_NOTIFICATION,
+      defaultJobOptions: {
+        removeOnComplete: true,
+      },
+    }),
+    BullBoardModule.forFeature({
+      name: JOB_NAMES.EMAIL_NOTIFICATION,
+      adapter: BullMQAdapter,
+    }),
   ],
-  providers: [EmailService],
+  providers: [EmailService, EmailWorker],
   exports: [EmailService],
 })
 export class EmailModule {}
--- src/notification/email/email.service.ts
+++ src/notification/email/email.service.ts
@@ -1,61 +1,41 @@
-import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from 'src/config/env.config';
 import { SendEMailDto } from './dto/send-email.dto';
 import { SendBulkEmailDto } from './dto/send-bulk-email.dto';
 import { SendExternalEmailDto } from './dto/send-external-email.dto';
+import { Queue } from 'bullmq';
+import { InjectQueue } from '@nestjs/bullmq';
+import { JOB_NAMES } from 'src/utils/constants';
 
 @Injectable()
 export class EmailService {
   constructor(
-    private mailerService: MailerService,
     private configService: ConfigService<EnvironmentVariables>,
+    @InjectQueue(JOB_NAMES.EMAIL_NOTIFICATION)
+    private readonly emailNotificationQueue: Queue,
   ) {}
-
-  emailLogger = new Logger(EmailService.name);
 
   async sendMailToUser(sendEmailDto: SendEMailDto) {
     const { user, subject, template, context } = sendEmailDto;
-    try {
-      await this.mailerService.sendMail({
-        to: user.email,
-        subject,
-        template,
-        context: context || {},
-      });
-      this.emailLogger.log(
-        `Email sent to user : ${user.email} with subject: ${subject}`,
-      );
-    } catch (error) {
-      this.emailLogger.error(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        `Failed to send email to user : ${user.email}: ${error.message}`,
-      );
-      throw error;
-    }
+    await this.emailNotificationQueue.add('send-email', {
+      to: user.email,
+      subject,
+      template,
+      context: context || {},
+    });
+    return true;
   }
 
   async sendBulkMailToUsers(sendBulkEmailDto: SendBulkEmailDto) {
     const { users, subject, template, context } = sendBulkEmailDto;
     const emailPromises = users.map(async (user) => {
-      try {
-        await this.mailerService.sendMail({
-          to: user.email,
-          subject,
-          template,
-          context: context || {},
-        });
-        this.emailLogger.log(
-          `Email sent to user : ${user.email} with subject: ${subject}`,
-        );
-      } catch (error) {
-        this.emailLogger.error(
-          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-          `Failed to send email to user : ${user.email}: ${error.message}`,
-        );
-        throw error;
-      }
+      await this.emailNotificationQueue.add('send-email', {
+        to: user.email,
+        subject,
+        template,
+        context: context || {},
+      });
     });
 
     await Promise.all(emailPromises);
@@ -63,22 +43,12 @@ export class EmailService {
 
   async sendExternalEmail(sendExternalEmailDto: SendExternalEmailDto) {
     const { recipientEmail, subject, template, context } = sendExternalEmailDto;
-    try {
-      await this.mailerService.sendMail({
-        to: recipientEmail,
-        subject,
-        template,
-        context: context || {},
-      });
-      this.emailLogger.log(
-        `Email sent to ${recipientEmail} with subject: ${subject}`,
-      );
-    } catch (error) {
-      this.emailLogger.error(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        `Failed to send email to ${recipientEmail}: ${error.message}`,
-      );
-      throw error;
-    }
+    await this.emailNotificationQueue.add('send-email', {
+      to: recipientEmail,
+      subject,
+      template,
+      context: context || {},
+    });
+    return true;
   }
 }
