143766eefdd401a83bc1d3913e7828a0cffded91
kastroud <johnsonolaolu@gmail.com>
2026-03-16 11:51:57 +0100

    feat: add Announcement entity with properties and relationships

--- src/announcement/announcement.module.ts
+++ src/announcement/announcement.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AnnouncementService } from './announcement.service';
 import { AnnouncementController } from './announcement.controller';
+import { AnnouncementGateway } from './announcement.gateway';
 
 @Module({
+  imports: [],
   controllers: [AnnouncementController],
-  providers: [AnnouncementService],
+  providers: [AnnouncementService, AnnouncementGateway],
 })
 export class AnnouncementModule {}
--- src/announcement/entities/announcement.entity.ts
+++ src/announcement/entities/announcement.entity.ts
@@ -1,1 +1,40 @@
-export class Announcement {}
+import { Landlord } from 'src/landlord/entities/landlord.entity';
+import { Property } from 'src/property/entities/property.entity';
+import { Unit } from 'src/property/entities/units.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  Relation,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Announcement extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @ManyToOne(() => Landlord)
+  landlord: Relation<Landlord>;
+
+  @ManyToOne(() => Property)
+  property: Relation<Property>;
+
+  @ManyToOne(() => Unit)
+  unit: Relation<Unit>;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
