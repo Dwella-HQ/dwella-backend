664c670ed5fa77bdd6ded589d80b58902eaf1421
kastroud <johnsonolaolu@gmail.com>
2026-03-16 16:03:47 +0100

    refactor: remove unused eslint directive from announcement service

--- src/announcement/announcement.service.ts
+++ src/announcement/announcement.service.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAnnouncementDto } from './dto/create-announcement.dto';
 import { UpdateAnnouncementDto } from './dto/update-announcement.dto';
