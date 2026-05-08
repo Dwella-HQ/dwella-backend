e4ab69e25a3cfd7c5a5a2316422a06c0a77510ce
kastroud <johnsonolaolu@gmail.com>
2026-05-08 09:15:55 +0100

    refactor(auth): update LoginDto to use non-null assertion and remove strong password validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,15 +1,10 @@
-import { IsEmail, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
-  email: string;
+  email!: string;
 
-  @IsStrongPassword({
-    minLength: 8,
-    minLowercase: 1,
-    minUppercase: 1,
-    minNumbers: 1,
-    minSymbols: 1,
-  })
-  password: string;
+  @IsString()
+  @IsNotEmpty()
+  password!: string;
 }
